b40992a4f1d94085fe8b8747d23b637ba886bc0a
Guillermo Julián <guillermo@gjulianm.me>
2025-11-02 15:26:09 +0100

    Fix export

--- datadog-agent/run.sh
+++ datadog-agent/run.sh
@@ -6,13 +6,13 @@
 #export DD_DOGSTATSD_NON_LOCAL_TRAFFIC= "true"
 
 # Execute the original Datadog Agent entrypoint
-DD_API_KEY="$(bashio::config 'api_key')" \
-DD_SITE="$(bashio::config 'site')" \
-DD_HOSTNAME="$(bashio::config 'hostname')" \
-DD_DOGSTATSD_NON_LOCAL_TRAFFIC="true" \
-DD_APM_ENABLED="false" \
-DOCKER_SOCKET_PATH="/run/docker.sock" \
-DOCKER_HOST="unix:///run/docker.sock" \
+export DD_API_KEY="$(bashio::config 'api_key')"
+export DD_SITE="$(bashio::config 'site')"
+export DD_HOSTNAME="$(bashio::config 'hostname')"
+export DD_DOGSTATSD_NON_LOCAL_TRAFFIC="true"
+export DD_APM_ENABLED="false"
+export DOCKER_SOCKET_PATH="/run/docker.sock"
+export DOCKER_HOST="unix:///run/docker.sock"
 
 # These are optional and applied last so we can override those set above if needed.
 for env_var in $(bashio::config 'env_vars|keys'); do
